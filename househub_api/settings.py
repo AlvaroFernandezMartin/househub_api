c2fc904ec624dcd8be6614350b049a7867116eb7
Alvaro <alvarofernandezmartintr@gmail.com>
2025-05-22 18:21:29 +0100

    .

--- househub_api/settings.py
+++ househub_api/settings.py
@@ -71,7 +71,8 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8080",  
+    "http://127.0.0.1:8080", 
+    'https://househubapi-production.up.railway.app/' ,
 ]
 
 
