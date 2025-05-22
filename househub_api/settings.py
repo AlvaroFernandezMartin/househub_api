620c599fb6c1b6e4c1d422a4766a0cd152efae3a
Alvaro <alvarofernandezmartintr@gmail.com>
2025-05-22 18:24:43 +0100

    .

--- househub_api/settings.py
+++ househub_api/settings.py
@@ -75,6 +75,11 @@ CORS_ALLOWED_ORIGINS = [
     'https://househubapi-production.up.railway.app/' ,
 ]
 
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.railway\.app$",
+    r"^http://127\.0\.0\.1:8080$",
+]
+
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'content-type',
